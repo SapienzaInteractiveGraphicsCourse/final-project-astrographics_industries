e2915ace6ec38761c9c6b02eb98db39267789173
alefa03 <nintendoads@gmail.com>
2026-08-14 01:06:31 +0200

    paths fix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,32 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  base: '/final-project-astrographics_industries/'
+  base: '/final-project-astrographics_industries/',
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'assets',
+          dest: ''
+        }
+      ]
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'sounds',
+          dest: ''
+        }
+      ]
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'textures',
+          dest: ''
+        }
+      ]
+    })
+  ]
 })
